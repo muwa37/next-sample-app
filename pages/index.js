f3dd17d1126f354d3bfdc98d12e0befce3c8ed8c
nuwa37 <yond41m3@icloud.com>
2024-03-27 12:04:29 +0500

    Fixed deps, added pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>main page</h1>
+    </div>
+  );
+};
+
+export default Index;
---
+++ pages/users.js
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div>
+      <h1>user list</h1>
+    </div>
+  );
+};
+
+export default Users;
